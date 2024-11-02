c796e21820d4691ecb86ee01a6da685350a97d5e
xillibit <xillibit@gmail.com>
2024-11-02 15:30:33 +0100

    [K6.3] "Show Images for Guests = No" block also the smleys #7856 #9756

--- src/libraries/kunena/src/BBCode/KunenaBBCode.php
+++ src/libraries/kunena/src/BBCode/KunenaBBCode.php
@@ -2703,7 +2703,7 @@ class KunenaBBCodeLibrary extends BBCodeLibrary
             ->set('alt', \count($matches) > 0 ? $altText : 0)
             ->set('canLink', $bbcode->autoLink_disable == 0);
 
-        if (Factory::getApplication()->getIdentity()->id == 0 && $this->config->showImgForGuest == 0) {
+            if (Factory::getApplication()->getIdentity()->id == 0 && $this->config->showImgForGuest == 0 && !preg_match('@media\/kunena\/emoticons@', $fileurl)) {
             // Hide between content from non registered users.
             return (string) $layout->set('title', Text::_('COM_KUNENA_SHOWIMGFORGUEST_HIDEIMG'))->setLayout('unauthorised');
         }
