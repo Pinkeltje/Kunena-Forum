444a79dbb22477e7f388e6613643d9fb5e72d952
Florian DAL FITTO <xillibit@gmail.com>
2024-12-20 15:26:14 +0100

    [K6.4] Create 6.4.0-2024-12-20_add_fa_socials.php

---
+++ src/admin/install/sql/updates/php/6.4.0-2024-12-20_add_fa_socials.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Kunena Component
+ *
+ * @package        Kunena.Installer
+ *
+ * @copyright      Copyright (C) 2008 - @currentyear@ Kunena Team. All rights reserved.
+ * @license        https://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link           https://www.kunena.org
+ **/
+defined('_JEXEC') or die();
+
+/**
+ * @param $parent
+ *
+ * @return array
+ * @throws Exception
+ * @since Kunena 6.4.0
+ */
+function kunena_640_2024_12_20_add_fa_socials($parent) { 
+
+}
