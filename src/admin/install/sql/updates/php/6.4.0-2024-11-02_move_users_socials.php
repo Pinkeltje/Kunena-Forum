e5fa6266b2c0c1572638d29e8697f448d31bff2a
xillibit <xillibit@gmail.com>
2025-02-08 16:52:55 +0100

    [K6.4] Test results update from 6.3.8 to 6.4.0 RC3 with huge amount of user #9814

--- src/admin/install/sql/updates/php/6.4.0-2024-11-02_move_users_socials.php
+++ src/admin/install/sql/updates/php/6.4.0-2024-11-02_move_users_socials.php
@@ -40,6 +40,28 @@ function chunk($array, $numRows, $size) {
  */
 function kunena_640_2024_11_02_move_users_socials($parent) {   
     $db     = Factory::getContainer()->get('DatabaseDriver');
+    
+    // Check if some columns of socials are missing in the table #__kunena_users
+    $db = Factory::getContainer()->get('DatabaseDriver');
+    $query = $db->getQuery(true);
+    $db->setQuery("SHOW COLUMNS FROM `#__kunena_users`");
+    $columnsKunenaUsers = $db->loadObjectList();
+    
+    $columsSocialsByDefault = ['icq', 'x_social', 'facebook', 'myspace', 'linkedin', 'linkedin_company', 'digg', 'skype', 'yim', 'google', 'github', 'microsoft', 'blogspot', 'flickr', 'bebo', 'instagram', 'qqsocial', 'qzone', 'weibo', 'wechat', 'vk', 'telegram', 'apple', 'vimeo', 'whatsapp', 'youtube', 'ok', 'pinterest', 'reddit', 'bsky_app'];
+    $columsToAvoid = ['userid', 'status', 'status_text', 'view', 'signature', 'moderator', 'banned', 'ordering', 'posts', 'avatar', 'timestamp', 'karma', 'group_id', 'uhits', 'personalText', 'gender', 'birthdate', 'location', 'websitename', 'websiteurl', 'rank', 'hideEmail', 'showOnline', 'canSubscribe', 'userListtime', 'thankyou', 'ip', 'socials'];
+    $columsSocialsInTable = [];
+    
+    foreach($columnsKunenaUsers as $column) {
+        if (array_search($column->Field, $columsToAvoid) === false) {
+            $columsSocialsInTable[] = $column->Field;
+        }
+    }
+    
+    if(count($columsSocialsInTable) > 0 ) {
+        $listSocialsColumns = $columsSocialsInTable;
+    } else {
+        $listSocialsColumns = $columsSocialsByDefault;
+    }
 
     // Get the number of lines in table #__kunena_users    
     $query  = $db->createQuery()
@@ -53,7 +75,7 @@ function kunena_640_2024_11_02_move_users_socials($parent) {
     $numRows = $db->getNumRows(); 
         
     $query  = $db->createQuery()
-        ->select($db->quoteName(array('userid','banned','digg','telegram','vk','microsoft','skype','x_social','facebook','myspace','google','github','linkedin','linkedin_company','instagram','qqsocial','blogspot','youtube','reddit','bsky_app','flickr','apple','qzone','vimeo','whatsapp','weibo','wechat','yim','pinterest','ok')))
+    ->select($db->quoteName($listSocialsColumns))
         ->from($db->quoteName('#__kunena_users'))
         ->where($db->quoteName('banned') . '= ' . $db->quote('1000-01-01 00:00:00')
     );
