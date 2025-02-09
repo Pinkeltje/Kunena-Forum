c9e1eb6c3417b0cd14c51b2b751fab3fa9807929
xillibit <xillibit@gmail.com>
2025-02-09 23:27:16 +0100

    [K6.4] Simplify the code to migrate socials like suggested by Pinkeltje

--- src/admin/install/sql/updates/php/6.4.0-2024-11-02_move_users_socials.php
+++ src/admin/install/sql/updates/php/6.4.0-2024-11-02_move_users_socials.php
@@ -47,7 +47,7 @@ function kunena_640_2024_11_02_move_users_socials($parent) {
     $db->setQuery("SHOW COLUMNS FROM `#__kunena_users`");
     $columnsKunenaUsers = $db->loadObjectList();
     
-    $columsSocialsByDefault = ['icq', 'x_social', 'facebook', 'myspace', 'linkedin', 'linkedin_company', 'digg', 'skype', 'yim', 'google', 'github', 'microsoft', 'blogspot', 'flickr', 'bebo', 'instagram', 'qqsocial', 'qzone', 'weibo', 'wechat', 'vk', 'telegram', 'apple', 'vimeo', 'whatsapp', 'youtube', 'ok', 'pinterest', 'reddit', 'bsky_app'];
+    $columsSocialsByDefault = ['x_social', 'facebook', 'myspace', 'linkedin', 'linkedin_company', 'digg', 'skype', 'yim', 'google', 'github', 'microsoft', 'blogspot', 'flickr', 'bebo', 'instagram', 'qqsocial', 'qzone', 'weibo', 'wechat', 'vk', 'telegram', 'apple', 'vimeo', 'whatsapp', 'youtube', 'ok', 'pinterest', 'reddit', 'bsky_app'];
     $columsToAvoid = ['userid', 'status', 'status_text', 'view', 'signature', 'moderator', 'banned', 'ordering', 'posts', 'avatar', 'timestamp', 'karma', 'group_id', 'uhits', 'personalText', 'gender', 'birthdate', 'location', 'websitename', 'websiteurl', 'rank', 'hideEmail', 'showOnline', 'canSubscribe', 'userListtime', 'thankyou', 'ip', 'socials'];
     $columsSocialsInTable = [];
     
@@ -87,117 +87,11 @@ function kunena_640_2024_11_02_move_users_socials($parent) {
     foreach (chunk($dataResults, $numRows, 100) as $line) {
         $result = ArrayHelper::toObject($line);
         $socials = KunenaUserSocials::getInstance($result->userid, false);
-            
-        if (isset($result->x_social)) {
-                $socials->x_social->value = $result->x_social;
-        }
-            
-        if (isset($result->facebook)) {
-                $socials->facebook->value = $result->facebook;
-        }
-            
-        if (isset($result->myspace)) {
-                $socials->myspace->value = $result->myspace;
-        }
-            
-        if (isset($result->linkedin)) {
-                $socials->linkedin->value = $result->linkedin;
-        }
-            
-        if (isset($result->linkedin_company)) {
-                $socials->linkedin_company->value = $result->linkedin_company;
-        }
-            
-        if (isset($result->digg)) {
-                $socials->digg->value = $result->digg;
-        }
-            
-        if (isset($result->skype)) {
-                $socials->skype->value = $result->skype;
-        }
-            
-        if (isset($result->yim)) {
-                $socials->yim->value = $result->yim;
-        }
-            
-        if (isset($result->google)) {
-                $socials->google->value = $result->google;
-        }
-            
-        if (isset($result->github)) {
-                $socials->github->value = $result->github;
-        }
-            
-        if (isset($result->microsoft)) {
-                $socials->microsoft->value = $result->microsoft;
-        }
-            
-        if (isset($result->blogspot)) {
-                $socials->blogspot->value = $result->blogspot;
-        }
-            
-        if (isset($result->flickr)) {
-                $socials->flickr->value = $result->flickr;
-        }
-            
-        if (isset($result->instagram)) {
-                $socials->instagram->value = $result->instagram;
-        }
-            
-        if (isset($result->qqsocial)) {
-                $socials->qqsocial->value = $result->qqsocial;
-        }
-            
-        if (isset($result->qzone)) {
-                $socials->qzone->value = $result->qzone;
-        }
-            
-        if (isset($result->weibo)) {
-                $socials->weibo->value = $result->weibo;
-        }
-            
-        if (isset($result->wechat)) {
-                $socials->wechat->value = $result->wechat;
-        }
-            
-        if (isset($result->vk)) {
-                $socials->vk->value = $result->vk;
-        }
-            
-        if (isset($result->telegram)) {
-                $socials->telegram->value = $result->telegram;
-        }
-            
-        if (isset($result->apple)) {
-                $socials->apple->value = $result->apple;
-        }
-            
-         if (isset($result->vimeo)) {
-                $socials->vimeo->value = $result->vimeo;
-        }
-            
-        if (isset($result->whatsapp)) {
-                $socials->whatsapp->value = $result->whatsapp;
-        }
-            
-        if (isset($result->youtube)) {
-                $socials->youtube->value = $result->youtube;
-        }
-            
-        if (isset($result->ok)) {
-                $socials->ok->value = $result->ok;
-        }
-            
-        if (isset($result->pinterest)) {
-                $socials->pinterest->value = $result->pinterest;
-        }
-            
-        if (isset($result->reddit)) {
-                $socials->reddit->value = $result->reddit;
-        }
-            
-        if (isset($result->bsky_app)) {
-                $socials->bluesky_app->value = $result->bsky_app;
+        
+        foreach ($columsSocialsByDefault as $socialColumn) {
+            if (isset($result->$socialColumn) && !empty($result->$socialColumn)) {
+                $socials->$socialColumn->value = $result->$socialColumn;
+            }
         }
             
         $socials->save();
