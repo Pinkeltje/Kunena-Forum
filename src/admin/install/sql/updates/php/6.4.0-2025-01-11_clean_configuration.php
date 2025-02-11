00223695724fc1713df2de68bad62df9c143e02f
xillibit <xillibit@gmail.com>
2025-02-11 22:42:41 +0100

    [K6.4] Get rid of obsolete config parameters on upgrade #9821

---
+++ src/admin/install/sql/updates/php/6.4.0-2025-01-11_clean_configuration.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Kunena Component
+ *
+ * @package        Kunena.Installer
+ *
+ * @copyright      Copyright (C) 2008 - @currentyear@ Kunena Team. All rights reserved.
+ * @license        https://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link           https://www.kunena.org
+ **/
+defined('_JEXEC') or die();
+
+use Joomla\CMS\Language\Text;
+use Kunena\Forum\Libraries\Factory\KunenaFactory;
+
+/**
+ * Clean in the configuration the leftovers of previous parameters which has been deleted since
+ * 
+ * @param $parent
+ *
+ * @return array
+ * @throws Exception
+ * @since Kunena 6.4.0
+ */
+function kunena_640_2025_02_11_clean_configuration($parent) {
+	$config = KunenaFactory::getConfig();
+	 
+	if (isset($config->emailHeadersizey)) {    
+	    unset($config->emailHeadersizey);
+	}
+	
+	if (isset($config->emailHeadersizex)) {	    
+	    unset($config->emailHeadersizex);
+	}
+	
+	if (isset($config->board_title)) {	    
+	    unset($config->board_title);
+	}
+	
+	if (isset($config->board_offline)) {	    
+	    unset($config->boardOffline);
+	}
+	
+	if (isset($config->offline_message)) {	    
+	    unset($config->offlineMessage);
+	}
+	
+	if (isset($config->userlist_posts)) {    
+	    unset($config->offlineMessage);
+	}
+	
+	if (isset($config->userlist_karma)) {	    
+	    unset($config->userlist_karma);
+	}
+	
+	// Save configuration
+	$config->save();	
+
+	return array('action' => '', 'name' => Text::_('COM_KUNENA_INSTALL_640_CLEAN_CONFIGURATION'), 'success' => true);
+}
